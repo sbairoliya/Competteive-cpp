0ac49f09bbf766b8d7125fd95def1da8e7ca91eb
Shivam <shivambairoliya@gmail.com>
2020-10-08 17:34:48 +0530

    new file

---
+++ CodeForces/GraknForcesD.cpp
@@ -0,0 +1,46 @@
+/*
+ * Developed By : Shivam Bairoliya
+ * Created on   : 10/8/2020 at 5:33 PM
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+using namespace chrono;
+
+typedef unsigned long long ull;
+typedef long double lld;
+typedef long long ll;
+
+#define vi vector<int>
+#define vl vector<long long>
+#define MOD 1000000007
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define readNumbersArray(n, arr) for(int i = 0; i < n; i++){cin>>arr[i];}
+#define readNumbersArray1(n, arr) for(int i = 0; i < n; i++){cin>>arr[i + 1];}
+
+/************************************* SOLUTION BELOW ***************************************/
+void solve() {
+
+}
+
+int32_t main() {
+    fastio();
+#ifndef ONLINE_JUDGE
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
+    freopen("../error.in", "w", stderr);
+    auto start = high_resolution_clock::now();
+#endif
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+#ifndef ONLINE_JUDGE
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(stop - start);
+    cout << "Time taken for execution: "
+         << duration.count() << " ms" << endl;
+#endif
+}
